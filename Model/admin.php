a8dc4b3a21c2fb11a0407525bce3a80002bfc053
Gentleviper1 <151238997+Gentleviper1@users.noreply.github.com>
2025-05-09 22:36:51 +0300

    .

---
+++ Model/admin.php
@@ -0,0 +1,14 @@
+<?php
+class Admin
+{
+    private $adminId;
+    private $Afirstname;
+    private $Alastname;
+    private $dob;
+    private $email;
+    private $role;
+    private $isActive;
+    private $phone;
+    private $password;
+}
+?>
