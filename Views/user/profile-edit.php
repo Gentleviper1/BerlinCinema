b4c1f36baafec842f5230a2110773e456af163df
Gentleviper1 <151238997+Gentleviper1@users.noreply.github.com>
2025-05-09 16:24:53 +0300

    ,,.

---
+++ Views/user/profile-edit.php
@@ -0,0 +1,261 @@
+<!DOCTYPE HTML>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title> Home Page</title>
+    <!-- Favicon Icon -->
+    <link rel="icon" type="image/png" href="assets/img/favcion.png" />
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css" media="all" />
+    <!-- Slick nav CSS -->
+    <link rel="stylesheet" type="text/css" href="assets/css/slicknav.min.css" media="all" />
+    <!-- Iconfont CSS -->
+    <link rel="stylesheet" type="text/css" href="assets/css/icofont.css" media="all" />
+    <!-- Owl carousel CSS -->
+    <link rel="stylesheet" type="text/css" href="assets/css/owl.carousel.css">
+    <!-- Popup CSS -->
+    <link rel="stylesheet" type="text/css" href="assets/css/magnific-popup.css">
+    <!-- Main style CSS -->
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css" media="all" />
+    <!-- Responsive CSS -->
+    <link rel="stylesheet" type="text/css" href="assets/css/responsive.css" media="all" />
+    <!--[if lt IE 9]>
+		  <script src="https://oss.maxcdn.com/php5shiv/3.7.3/php5shiv.min.js"></script>
+		  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+</head>
+
+<body>
+    <!-- Page loader -->
+    <div id="preloader"></div>
+    <!-- header section start -->
+    <header class="header">
+        <div class="container">
+            <div class="header-area">
+                <div class="logo">
+                    <a href="index.php"><img src="assets/img/logo.png" alt="logo" /></a>
+                </div>
+                <div class="header-right">
+                    <form action="#">
+                        <select>
+                            <option value="Movies">Tittle</option>
+                            <option value="Movies">Category</option>
+                            <option value="Movies">Genre</option>
+                        </select>
+                        <input type="text" />
+
+                    </form>
+                    <ul>
+
+                        <li><a class="" href="../../Auth/login.php">Login</a></li>
+                    </ul>
+                </div>
+                <div class="menu-area">
+                    <div class="responsive-menu"></div>
+                    <div class="mainmenu">
+                        <ul id="primary-menu">
+                            <li><a class="active" href="index.php">Home</a></li>
+                            <!--<li><a href="movies.php">Movies</a></li>-->
+                            <!-- <li><a href="celebrities.php">CelebritiesList</a></li> -->
+                            <li><a href="top-movies.php">Top Movies</a></li>
+                            <li><a href="blog.php">News</a></li>
+                            <li><a href="movies-details.php">Movie Details <i class="icofont icofont-simple-down"></i></a>
+
+                            </li>
+
+                            <a class="theme-btn " href="../../Auth/login.php"><i class="icofont icofont-ticket"></i> Tickets</a>
+
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+
+
+    </div>
+    </div>
+    <!-- header section end -->
+    <section class="breadcrumb-area">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+
+                    <div class="card card-default">
+                        <div class="card-header">
+                            <h2 class="mb-5 text-color ">Account Settings</h2>
+
+                        </div>
+
+                        <div class="card-body">
+
+                            <form>
+                                <div class="row mb-2">
+                                    <div class="col-lg-6">
+                                        <div class="form-group">
+                                            <label for="firstName">First name</label>
+                                            <input type="text" class="form-control" id="firstName">
+                                        </div>
+                                    </div>
+
+                                    <div class="col-lg-6">
+                                        <div class="form-group">
+                                            <label for="lastName">Last name</label>
+                                            <input type="text" class="form-control" id="lastName">
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="form-group mb-4">
+                                    <label for="userName">User name</label>
+                                    <input type="text" class="form-control" id="userName">
+                                    <span class="d-block mt-1">Accusamus nobis at omnis consequuntur culpa tempore saepe animi.</span>
+                                </div>
+
+                                <div class="form-group mb-4">
+                                    <label for="email">Email</label>
+                                    <input type="email" class="form-control" id="email">
+                                </div>
+
+                                <div class="form-group mb-4">
+                                    <label for="newPassword">New password</label>
+                                    <input type="password" class="form-control" id="newPassword">
+                                </div>
+
+                                <div class="form-group mb-4">
+                                    <label for="conPassword">Confirm password</label>
+                                    <input type="password" class="form-control" id="conPassword">
+                                </div>
+
+                                <div class="d-flex justify-content-end mt-6">
+                                    <button type="submit" class="btn btn-primary mb-2 btn-pill">Update Profile</button>
+                                </div>
+
+                            </form>
+                        </div>
+                    </div>
+
+
+
+
+                </div>
+
+            </div>
+        </div>
+
+
+
+        </div>
+        </div>
+        </div>
+    </section><!-- breadcrumb area end -->
+
+
+
+    <!-- footer section start -->
+    <footer class="footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-2 col-sm-6">
+                    <div class="widget">
+                        <img src="assets/img/logo.png" alt="about" />
+                        <p> Mühlenstraße, 10243 Berlin, Germany</p>
+                        <h6><span>Call us: </span>(+20)1069930446</h6>
+                    </div>
+
+                </div>
+                <div class="col-lg-2 col-sm-6">
+                    <div class="widget">
+                        <h4>Our Company</h4>
+                        <ul>
+                            <li><a href="">Our Brands</a></li>
+                            <li><a href="#">Contact Us</a></li>
+                            <li><a href="#">FAQs</a></li>
+                            <li><a href="#">Berlin Cinema Investor Connect </a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-2 col-sm-6">
+                    <div class="widget">
+                        <h4>Movies</h4>
+                        <ul>
+                            <li><a href="">Movies</a></li>
+                            <li><a href="#">Theatres</a></li>
+                            <li><a href="#">Ratings Information</a></li>
+                            <li><a href="#">IMAX at Berlin Cinema </a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-2 col-sm-6">
+                    <div class="widget">
+                        <h4>Legal</h4>
+                        <ul>
+                            <li><a href="#">Terms of Use</a></li>
+                            <li><a href="#">Privacy Policy</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-2 col-sm-6">
+                    <div class="widget">
+                        <h4>Account</h4>
+                        <ul>
+                            <li><a href="">My Account</a></li>
+                            <li><a href="#">Watchlist</a></li>
+                            <li><a href="#">Collections</a></li>
+                            <li><a href="#">User Guide</a></li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="col-lg-2 col-sm-6">
+                    <div class="widget">
+                        <h4>Newsletter</h4>
+                        <p>Subscribe to our newsletter system now to get latest news from us.</p>
+                        <form action="#">
+                            <input type="text" placeholder="Enter your email.." />
+                            <button>SUBSCRIBE NOW</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <hr />
+        </div>
+        <div class="copyright">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-6 text-center text-lg-left">
+                        <div class="copyright-content">
+                            <p><a target="_blank" href="index.php">Berlin Cinema</a></p>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 text-center text-lg-right">
+                        <div class="copyright-content">
+                            <a href="#" class="scrollToTop">
+                                Back to top<i class="icofont icofont-arrow-up"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer><!-- footer section end -->
+    <!-- jquery main JS -->
+    <script src="assets/js/jquery.min.js"></script>
+    <!-- Bootstrap JS -->
+    <script src="assets/js/bootstrap.min.js"></script>
+    <!-- Slick nav JS -->
+    <script src="assets/js/jquery.slicknav.min.js"></script>
+    <!-- owl carousel JS -->
+    <script src="assets/js/owl.carousel.min.js"></script>
+    <!-- Popup JS -->
+    <script src="assets/js/jquery.magnific-popup.min.js"></script>
+    <!-- Isotope JS -->
+    <script src="assets/js/isotope.pkgd.min.js"></script>
+    <!-- main JS -->
+    <script src="assets/js/main.js"></script>
+</body>
+
+</html>
