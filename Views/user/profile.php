3af9b74b523d72cd7c967bcab0f3d6ec14d5f647
Gentleviper1 <151238997+Gentleviper1@users.noreply.github.com>
2025-05-09 17:48:17 +0300

    lo

--- Views/user/profile.php
+++ Views/user/profile.php
@@ -3,16 +3,41 @@
 
 <head>
   <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>User Profile - Berlin Cinema</title>
   <!-- Bootstrap CSS -->
   <link rel="stylesheet" href="assets/css/bootstrap.min.css" />
   <link rel="stylesheet" href="assets/css/icofont.css" />
+
+
+
+  <title>Moviepoint - Online Movie,Vedio and TV Show php Template</title>
+  <!-- Favicon Icon -->
+  <link rel="icon" type="image/png" href="assets/img/favcion.png" />
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css" media="all" />
+  <!-- Slick nav CSS -->
+  <link rel="stylesheet" type="text/css" href="assets/css/slicknav.min.css" media="all" />
+  <!-- Iconfont CSS -->
+  <link rel="stylesheet" type="text/css" href="assets/css/icofont.css" media="all" />
+  <!-- Owl carousel CSS -->
+  <link rel="stylesheet" type="text/css" href="assets/css/owl.carousel.css">
+  <!-- Popup CSS -->
+  <link rel="stylesheet" type="text/css" href="assets/css/magnific-popup.css">
+  <!-- Main style CSS -->
+  <link rel="stylesheet" type="text/css" href="assets/css/style.css" media="all" />
+  <!-- Responsive CSS -->
+  <link rel="stylesheet" type="text/css" href="assets/css/responsive.css" media="all" />
+  <!--[if lt IE 9]>
+		  <script src="https://oss.maxcdn.com/php5shiv/3.7.3/php5shiv.min.js"></script>
+		  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
   <style>
     body {
       margin: 0;
       padding: 0;
-      background: url('assets/img/RogueOne.jpg') no-repeat center center fixed;
+      background: url('assets/img/backk.png') no-repeat center center fixed;
       background-size: cover;
       color: white;
       font-family: 'Segoe UI', sans-serif;
@@ -81,111 +106,192 @@
 
 <body>
 
-  <!-- Header -->
-  <header class="header bg-dark py-3">
-    <div class="container d-flex justify-content-between align-items-center">
-      <div class="logo">
-        <a href="index.php"><img src="assets/img/logo.png" alt="logo" height="40" /></a>
-      </div>
-      <div class="d-flex align-items-center">
-        <form class="me-3 d-flex">
-          <select class="form-select me-2">
-            <option>Title</option>
-            <option>Category</option>
-            <option>Genre</option>
-          </select>
-          <input type="text" class="form-control" placeholder="Search..." />
-        </form>
-        <a href="../../Auth/login.php" class="btn btn-outline-light">Login</a>
+  <!-- Page loader -->
+  <div id="preloader"></div>
+  <!-- header section start -->
+  <header class="header">
+    <div class="container">
+      <div class="header-area">
+        <div class="logo">
+          <a href="index.php"><img src="assets/img/logo.png" alt="logo" /></a>
+        </div>
+        <div class="header-right">
+          <form action="#">
+            <select>
+              <option value="Movies">Tittle</option>
+              <option value="Movies">Category</option>
+              <option value="Movies">Genre</option>
+            </select>
+
+            <input type="text" />
+
+          </form>
+          <ul>
+
+            <li><a class="" href="index.php">Logout</a></li>
+          </ul>
+        </div>
+        <div class="menu-area">
+          <div class="responsive-menu"></div>
+          <div class="mainmenu">
+            <ul id="primary-menu">
+              <li><a class="active" href="index-2.php">Home</a></li>
+              <li><a href="movies.php">Movies</a></li>
+              <li><a href="showtimes.php">showtimes</a></li>
+              <li><a href="profile.php">profile</a></li>
+              <li><a href="blog-2.php">News</a></li>
+              <li><a href="movies-details-2.php">Movie Details <i class="icofont icofont-simple-down"></i></a>
+
+              </li>
+              <a class="theme-btn " href="tickets.php"><i class="icofont icofont-ticket"></i> Book Now</a>
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
   </header>
 
-  <!-- Profile Form -->
-  <div class="container profile-box text-center">
-    <img src="https://randomuser.me/api/portraits/men/75.jpg" alt="User Avatar" class="profile-avatar" />
-    <h2 class="mb-4">Edit Your Profile</h2>
-
-    <form method="POST" action="#">
-      <div class="row mb-3">
-        <div class="col-md-6">
-          <label class="form-label">First Name</label>
-          <input type="text" class="form-control" name="first_name" placeholder="Enter your first name" required />
-        </div>
-        <div class="col-md-6">
-          <label class="form-label">Last Name</label>
-          <input type="text" class="form-control" name="last_name" placeholder="Enter your last name" required />
-        </div>
-      </div>
-      <div class="mb-3">
-        <label class="form-label">Email</label>
-        <input type="email" class="form-control" name="email" placeholder="Enter your email" required />
-      </div>
-      <div class="mb-3">
-        <label class="form-label">Birthdate</label>
-        <input type="date" class="form-control" name="birthdate" required />
-      </div>
-      <button type="submit" class="btn btn-primary px-4">Save Profile</button>
-    </form>
   </div>
-
-  <!-- Footer -->
-  <footer>
+  </div>
+  <!--header section end -->
+  <section class="breadcrumb-area1">
     <div class="container">
       <div class="row">
-        <div class="col-md-2">
-          <img src="assets/img/logo.png" alt="logo" width="120" />
-          <p>Mühlenstraße, 10243 Berlin, Germany</p>
-          <h6><span>Call us: </span>(+20)1069930446</h6>
-        </div>
-        <div class="col-md-2">
-          <h4>Our Company</h4>
-          <ul class="list-unstyled">
-            <li><a href="#">Our Brands</a></li>
-            <li><a href="#">Contact Us</a></li>
-            <li><a href="#">FAQs</a></li>
-            <li><a href="#">Investors</a></li>
-          </ul>
-        </div>
-        <div class="col-md-2">
-          <h4>Movies</h4>
-          <ul class="list-unstyled">
-            <li><a href="#">Movies</a></li>
-            <li><a href="#">Theatres</a></li>
-            <li><a href="#">Ratings</a></li>
-            <li><a href="#">IMAX</a></li>
-          </ul>
-        </div>
-        <div class="col-md-2">
-          <h4>Legal</h4>
-          <ul class="list-unstyled">
-            <li><a href="#">Terms</a></li>
-            <li><a href="#">Privacy</a></li>
-          </ul>
-        </div>
-        <div class="col-md-2">
-          <h4>Account</h4>
-          <ul class="list-unstyled">
-            <li><a href="#">My Account</a></li>
-            <li><a href="#">Watchlist</a></li>
-            <li><a href="#">Collections</a></li>
-          </ul>
-        </div>
-        <div class="col-md-2">
-          <h4>Newsletter</h4>
-          <form>
-            <input type="text" class="form-control mb-2" placeholder="Email..." />
-            <button class="btn btn-sm btn-primary">Subscribe</button>
+
+        <!-- Profile Form -->
+        <div class="container profile-box text-center">
+          <img src="https://randomuser.me/api/portraits/men/75.jpg" alt="User Avatar" class="profile-avatar" />
+          <h2 class="mb-4">Edit Your Profile</h2>
+
+          <form method="POST" action="#">
+            <div class="row mb-3">
+              <div class="col-md-6">
+                <label class="form-label">First Name</label>
+                <input type="text" class="form-control" name="first_name" placeholder="Enter your first name" required />
+              </div>
+              <div class="col-md-6">
+                <label class="form-label">Last Name</label>
+                <input type="text" class="form-control" name="last_name" placeholder="Enter your last name" required />
+              </div>
+            </div>
+            <div class="mb-3">
+              <label class="form-label">Email</label>
+              <input type="email" class="form-control" name="email" placeholder="Enter your email" required />
+            </div>
+            <div class="mb-3">
+              <label class="form-label">Birthdate</label>
+              <input type="date" class="form-control" name="birthdate" required />
+            </div>
+            <button type="submit" class="btn btn-primary px-4">Save Profile</button>
           </form>
         </div>
       </div>
-      <hr class="bg-white" />
-      <p class="text-center">&copy; 2025 Berlin Cinema. All rights reserved.</p>
     </div>
-  </footer>
+    </div>
 
-  <!-- Scripts -->
+  </section>
+
+  <!-- footer section start -->
+  <footer class="footer">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-2 col-sm-6">
+          <div class="widget">
+            <img src="assets/img/logo.png" alt="about" />
+            <p> Mühlenstraße, 10243 Berlin, Germany</p>
+            <h6><span>Call us: </span>(+20)1069930446</h6>
+          </div>
+
+        </div>
+        <div class="col-lg-2 col-sm-6">
+          <div class="widget">
+            <h4>Our Company</h4>
+            <ul>
+              <li><a href="">Our Brands</a></li>
+              <li><a href="#">Contact Us</a></li>
+              <li><a href="#">FAQs</a></li>
+              <li><a href="#">Berlin Cinema Investor Connect </a></li>
+            </ul>
+          </div>
+        </div>
+        <div class="col-lg-2 col-sm-6">
+          <div class="widget">
+            <h4>Movies</h4>
+            <ul>
+              <li><a href="">Movies</a></li>
+              <li><a href="#">Theatres</a></li>
+              <li><a href="#">Ratings Information</a></li>
+              <li><a href="#">IMAX at Berlin Cinema </a></li>
+            </ul>
+          </div>
+        </div>
+        <div class="col-lg-2 col-sm-6">
+          <div class="widget">
+            <h4>Legal</h4>
+            <ul>
+              <li><a href="#">Terms of Use</a></li>
+              <li><a href="#">Privacy Policy</a></li>
+            </ul>
+          </div>
+        </div>
+        <div class="col-lg-2 col-sm-6">
+          <div class="widget">
+            <h4>Account</h4>
+            <ul>
+              <li><a href="">My Account</a></li>
+              <li><a href="#">Watchlist</a></li>
+              <li><a href="#">Collections</a></li>
+              <li><a href="#">User Guide</a></li>
+            </ul>
+          </div>
+        </div>
+
+        <div class="col-lg-2 col-sm-6">
+          <div class="widget">
+            <h4>Newsletter</h4>
+            <p>Subscribe to our newsletter system now to get latest news from us.</p>
+            <form action="#">
+              <input type="text" placeholder="Enter your email.." />
+              <button>SUBSCRIBE NOW</button>
+            </form>
+          </div>
+        </div>
+      </div>
+      <hr />
+    </div>
+    <div class="copyright">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-6 text-center text-lg-left">
+            <div class="copyright-content">
+              <p><a target="_blank" href="index.php">Berlin Cinema</a></p>
+            </div>
+          </div>
+          <div class="col-lg-6 text-center text-lg-right">
+            <div class="copyright-content">
+              <a href="#" class="scrollToTop">
+                Back to top<i class="icofont icofont-arrow-up"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer><!-- footer section end -->
+  <!-- jquery main JS -->
+  <script src="assets/js/jquery.min.js"></script>
+  <!-- Bootstrap JS -->
   <script src="assets/js/bootstrap.min.js"></script>
+  <!-- Slick nav JS -->
+  <script src="assets/js/jquery.slicknav.min.js"></script>
+  <!-- owl carousel JS -->
+  <script src="assets/js/owl.carousel.min.js"></script>
+  <!-- Popup JS -->
+  <script src="assets/js/jquery.magnific-popup.min.js"></script>
+  <!-- Isotope JS -->
+  <script src="assets/js/isotope.pkgd.min.js"></script>
+  <!-- main JS -->
+  <script src="assets/js/main.js"></script>
 </body>
 
 </html>
